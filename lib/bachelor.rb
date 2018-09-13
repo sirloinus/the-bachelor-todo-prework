cdaa82f026828016a8f8d2cc8b8cd27e3e34cd81
Amalie S <amaliesmidth@gmail.com>
2018-09-13 14:41:15 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,5 +45,7 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  contestants_ages = 0
+  data.each do |season, contestants|
+  if data[]
 end
